a93d6defd05d2a3b35c8e18115a9ec07da0308a1
phontomn <1162395603@qq.com>
2019-05-27 12:07:29 +0800

    1

--- js/tesk0002.js
+++ js/tesk0002.js
@@ -534,7 +534,7 @@ window.onload = function() {
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
-                htxt.innerHTML = t;
+                htxt.innerHTML = this.responseText;
             }
         }
         xhttp.open("POST", "js_hobby.txt", true);
