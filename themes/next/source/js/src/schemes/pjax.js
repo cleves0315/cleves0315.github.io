164f06b541b06dc6355981497794ac5b5c902bf0
Cleves <cleves0315@foxmail.com>
2021-07-26 19:24:25 +0800

    refactor: cnd vendors.js

--- themes/next/source/js/src/schemes/pjax.js
+++
@@ -1,7 +0,0 @@
-require('jquery-pjax');
-console.log('===pjax===')
-$(document).pjax('a:not(.fancybox):not([target="_blank"])', '#main', {
-  scrollTo: $('.main').position().top - 60,
-  fragment: '#main',
-  timeout: 5000,
-});
